604d91a5def86b3831ae902c917993d1d289c237
Emmanuel <ogankpaugo@gmail.com>
2021-09-26 23:57:40 +0100

    OTM first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function btn(){
+    document.getElementById('nav').style.display = "block"
+    document.getElementById('open').style.display = "none"
+    document.getElementById('cls').style.display = "block"
+}
+function cls(){
+    document.getElementById('nav').style.display = "none"
+    document.getElementById('open').style.display = "block"
+    document.getElementById('cls').style.display = "none"
+}
